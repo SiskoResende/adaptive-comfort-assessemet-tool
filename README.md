45e088870c702c3213657fb8f5a2c37aa9cc49dc
Sisko Resende <vfpresende@gmail.com>
2022-07-18 23:05:28 -0300

    ligeira arrumada

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# COMFORT ASSESSMENT TOOL (A-CAT)
+# COMFORT ASSESSMENT TOOL (CAT)
+
+Módulo para auxiliar na avaliação da simulação do conforto térmico
 
 Need python 3.8 and pandas 1.3.2
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from lib import comfortassessmenttool
 
 print('Hello World')
+
+'''
+PATH = caminho. registra a pasta de trabalho e muda para ela
+EPOutput = caminho. pode pegar um arquivo .csv ou todos csv da pasta
+'''
+
